3f38473535a58d0d4e7c87b1ba3d09290fde9486
Greg Ross <gregandrewross79@gmail.com>
2024-05-10 18:28:44 +0100

    Initial commit

---
+++ TrackingSpotter/TrackingSpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingSpotterApp.swift
+//  TrackingSpotter
+//
+//  Created by Greg Ross on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackingSpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
