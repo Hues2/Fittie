dd917e8d20a856446d13ab565ce29376035d4731
Greg Ross <gregandrewross79@gmail.com>
2024-05-10 18:31:00 +0100

    Added a home view

---
+++ TrackingSpotter/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  TrackingSpotter
+//
+//  Created by Greg Ross on 10/05/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
