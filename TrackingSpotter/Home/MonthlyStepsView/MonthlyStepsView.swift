77e24e133d32e272280916280a4fac9a25405916
Greg Ross <gregandrewross79@gmail.com>
2024-05-17 00:07:32 +0100

    Updated the design for the bar chart and added the total monthly steps amount

--- TrackingSpotter/Home/MonthlyStepsView/MonthlyStepsView.swift
+++ TrackingSpotter/Home/MonthlyStepsView/MonthlyStepsView.swift
@@ -6,25 +6,41 @@ struct MonthlyStepsView: View {
     let stepGoal : Int
     
     var body: some View {
-        chart()
-            .padding(.top, 4)
+        VStack {
+            Text("Total: \(monthlySteps.reduce(0, { $0 + $1.steps }))")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            chart()                
+        }
     }
 }
 
 private extension MonthlyStepsView {
     @ViewBuilder func chart() -> some View {
-        let max = monthlySteps.max { item1, item2 in
-            return item2.steps > item1.steps
-        }?.steps ?? 0
-        
         Chart {
+            RuleMark(y: .value("Step Goal", stepGoal))
+                .foregroundStyle(Color.pink.gradient)
+                .lineStyle(StrokeStyle(lineWidth: 2, dash: [5]))
+            
             ForEach(monthlySteps) { dailyStep in
                 BarMark(x: .value(dailyStep.date.formatted(), dailyStep.date, unit: .day),
                         y: .value("Steps", dailyStep.animate ? dailyStep.steps : 0))
-                .foregroundStyle((dailyStep.steps >= stepGoal) ? Color.accent : Color.red)
+                .foregroundStyle((dailyStep.steps >= stepGoal) ? Color.accent.gradient : Color.pink.gradient)
+                .cornerRadius(2)
             }
         }
-        .chartYScale(domain: 0...(max + 1000))
+        .chartYScale(domain: 0...(getMax() + 1000))
+        .chartYAxis {
+            AxisMarks { mark in
+                AxisValueLabel()
+                AxisGridLine(centered: false, stroke: .init(lineWidth: 1, dash: [5]))
+                    .foregroundStyle(Color.lightGray)
+                    
+            }
+        }
+        .chartXAxis(.hidden)
         .onAppear {
             for (index, _) in monthlySteps.enumerated() {
                 withAnimation(.easeInOut(duration: 0.8).delay(Double(index) * 0.03)) {
@@ -32,18 +48,36 @@ private extension MonthlyStepsView {
                 }
             }
         }
-        .clipped()
+    }
+    
+    func getMax() -> Int {
+        return monthlySteps.max { item1, item2 in
+            return item2.steps > item1.steps
+        }?.steps ?? 0
     }
 }
 
 #Preview {
-    MonthlyStepsView(monthlySteps: [DailyStep(date: Date(), steps: 11632),
-                                  DailyStep(date: Date(), steps: 1789),
-                                  DailyStep(date: Date(), steps: 1686),
-                                  DailyStep(date: Date(), steps: 1478),
-                                  DailyStep(date: Date(), steps: 1397),
-                                  DailyStep(date: Date(), steps: 922),
-                                  DailyStep(date: Date(), steps: 2408)],
-                   stepGoal: 1400)
-    .frame(width: 300)
+    func randomStepCount() -> Int {
+        return Int.random(in: 200...10_000)
+    }
+
+    // Function to generate an array of DailyStep objects
+    func generateMockData() -> [DailyStep] {
+        var mockData: [DailyStep] = []
+        let currentDate = Date()
+
+        // Generate data for the last 30 days
+        for i in 0..<30 {
+            let date = Calendar.current.date(byAdding: .day, value: -i, to: currentDate)!
+            let steps = randomStepCount()
+            let dailyStep = DailyStep(date: date, steps: steps)
+            mockData.append(dailyStep)
+        }
+
+        return mockData
+    }
+    return MonthlyStepsView(monthlySteps: generateMockData(),
+                            stepGoal: Int.random(in: 500...10_000))
+    .frame(height: 275)
 }
