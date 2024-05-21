eae748cd4cd9f578632a7d1985094cc9d5ad477e
Greg Ross <gregandrewross79@gmail.com>
2024-05-21 23:18:15 +0100

    Reformatted the save action method

--- Fittie/Home/WeightView/WeightSheets/LogWeightView.swift
+++ Fittie/Home/WeightView/WeightSheets/LogWeightView.swift
@@ -16,6 +16,10 @@ private extension LogWeightView {
     func saveAction() {        
         guard let weight else { return }
         let newWeight = Weight(date: .now.startOfDay, kg: weight)
+        context.insert(newWeight)
+    }
+    
+    func addMockWeights() {
         let newWeight1 = Weight(date: Date.getDayFrom(date: .now, days: 1)!, kg: 87)
         let newWeight2 = Weight(date: Date.getDayFrom(date: .now, days: 3)!, kg: 83)
         let newWeight3 = Weight(date: Date.getDayFrom(date: .now, days: 5)!, kg: 90)
@@ -32,19 +36,21 @@ private extension LogWeightView {
         let newWeight11 = Weight(date: Date.getDayFrom(date: .now, days: 270)!, kg: 83)
         let newWeight12 = Weight(date: Date.getDayFrom(date: .now, days: 365)!, kg: 90)
         
-        context.insert(newWeight)
         context.insert(newWeight1)
         context.insert(newWeight2)
         context.insert(newWeight3)
+        
         context.insert(newWeight4)
         context.insert(newWeight5)
         context.insert(newWeight6)
-//            context.insert(newWeight7)
-//            context.insert(newWeight8)
-//            context.insert(newWeight9)
-//            context.insert(newWeight10)
-//            context.insert(newWeight11)
-//            context.insert(newWeight12)
+        
+        context.insert(newWeight7)
+        context.insert(newWeight8)
+        context.insert(newWeight9)
+        
+        context.insert(newWeight10)
+        context.insert(newWeight11)
+        context.insert(newWeight12)
     }
 }
 
