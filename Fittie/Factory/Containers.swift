3acacb2f31d9745a84d10d3edc9af63b4bc52494
Greg Ross <gregandrewross79@gmail.com>
2024-06-18 21:10:53 +0100

    Fixed issue with multiple instances of the weight goal manager

--- Fittie/Factory/Containers.swift
+++ Fittie/Factory/Containers.swift
@@ -5,15 +5,18 @@ extension Container {
     // MARK: - HealthKit Manager
     var healthKitManager : Factory<HealthKitManager> {
         self { HealthKitManager() }
+            .singleton
     }
     
     // MARK: - Step Goal Manager
     var stepGoalManager : Factory<StepGoalManager> {
         self { StepGoalManager() }
+            .singleton
     }
     
     // MARK: - Weight Goal Manager
     var weightGoalManager : Factory<WeightGoalManager> {
         self { WeightGoalManager() }
+            .singleton
     }
 }
